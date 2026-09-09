fca7daf3b8249c65c9f5995b9b1180ac8a84fab0
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-09 09:04:30 +0300

    Modified the password hashing method and tested on main file

--- Models/User.py
+++ Models/User.py
@@ -1,16 +1,17 @@
 import hashlib
+import random
 
 class User:
-    def __init__(self, user_id, name, email, phone, role, password):
-        self.user_id = user_id
+    def __init__(self, name, email, phone, password):
+        self.user_id = random.randint(1000, 9999)
         self.name = name
         self.email = email
         self.phone_number = phone
-        self.role = role
-        self._password_hash = self._hash_password(password)
+        self.role = None
+        self._password = password
 
-    def _hash_password(self, password):
-        return hashlib.sha256(password.encode()).hexdigest()
+    def _hash_password(self):
+        return hashlib.sha256(self._password.encode()).hexdigest()
 
-    def verify_password(self, password):
-        return self._password_hash == self._hash_password(password)
+    # def verify_password(self, password):
+    #     return self._password_hash == self._hash_password(password)
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello Kappa Alpha")
+from Models.User import User
+
+user1 = User("Telvin", "telvin@gmail.com", "2345432", "dcdceee")
+user1._hash_password()
