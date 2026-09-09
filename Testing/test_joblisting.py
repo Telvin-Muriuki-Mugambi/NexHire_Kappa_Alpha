c1f9e4d63eeb87adaba3cf9e3ce3c4c7267c4107
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-09 19:08:58 +0300

    Test to approve or reject the job listing by changing the status from PENDING to APPROVED or REJECTED respectively

--- Testing/test_joblisting.py
+++ Testing/test_joblisting.py
@@ -18,3 +18,15 @@ def listing():
 #Test to ensure all the job listings created result to a pending status
 def test_new_listing_defaults_to_pending(listing):
 	assert listing.status == "PENDING"
+
+#Test to approve of Job Listing and changing status from PENDING to APPROVE
+def test_approve_changes_status(listing):
+	listing.approve()
+
+	assert listing.status == "APPROVED"
+
+#Test to REJECT of Job Listing and changing status from PENDING to REJECTED
+def test_reject_changes_status(listing):
+	listing.reject()
+
+	assert listing.status == "REJECTED"
