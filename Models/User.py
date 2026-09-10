f8c764fff86a193ea3f6781a7369f7576d630f89
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-10 09:50:43 +0300

    Beginning test file development

--- Models/User.py
+++ Models/User.py
@@ -2,8 +2,8 @@ import hashlib
 import random
 
 class User:
-    def __init__(self, name, email, phone, password):
-        self.user_id = random.randint(1000, 9999)
+    def __init__(self, name, email, phone, password, user_id=None):
+        self.user_id = user_id if user_id is not None else random.randint(1000, 9999)
         self.name = name
         self.email = email
         self.phone_number = phone
