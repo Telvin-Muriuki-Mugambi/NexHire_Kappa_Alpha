5ea773b65a5eedc09945ff00393467767f90ffee
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-10 10:14:59 +0300

    Test case to check existence of the JSON file

--- Testing/test_datamanager.py
+++ Testing/test_datamanager.py
@@ -1,2 +1,14 @@
-#Test file for the data manager
-print("data manager test")
+from pathlib import Path
+#Used for handling file paths in Python
+
+class DataManager:
+    # Test file used to test for persistence users and job listings as JSON.
+
+    #Test case to check exisitence of files
+    def __init__(self, data_dir):
+        self.data_dir = Path(data_dir)
+        self.data_dir.mkdir(parents=True, exist_ok=True)
+        self.users_file = self.data_dir / "users.json"
+        self.jobs_file = self.data_dir / "jobs.json"
+        self._ensure_file(self.users_file)
+        self._ensure_file(self.jobs_file)
