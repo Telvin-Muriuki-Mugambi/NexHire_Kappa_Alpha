479a2ba6f63948c1c3454bbe71a440ffcd908f98
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-11 15:06:57 +0300

    Setting up logic to check if the user proceeds as a job seeker or an an employer

--- flow/Register.py
+++ flow/Register.py
@@ -1,1 +1,12 @@
-print("Page dedicated to registration")
+from Models.Auth import Auth, AuthenticationError, AuthorizationError
+#Importing the Auth class
+
+def register(): 
+    user_role = input("Would you like to proceed as a Job Seeker or Employer\n Job Seeker type (JB)\n Employer type(E)\n: ")
+
+    if user_role == "JB" or "jb":
+        pass
+    elif user_role == "E" or "e":
+        pass
+    else:
+        pass
