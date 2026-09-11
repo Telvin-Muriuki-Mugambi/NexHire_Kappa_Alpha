9f138e7cdd0abbd61e8cae80ff90c49fa35f55f5
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-11 12:04:47 +0300

    Initialized the auth class and added attributes

--- Models/Auth.py
+++ Models/Auth.py
@@ -4,3 +4,12 @@
 # TODO: Login of a user
 # TODO: Role Based Access [Job seeker, Employer, Admin]
 # TODO: Password Hashing 
+
+class Auth:
+    ROLES = {"ADMIN", "JOB_SEEKER", "EMPLOYER"}
+
+    def __init__(self):
+        #Used to save the user's data
+        self.user = {}
+        #Set the user session
+        self.current_user = None
