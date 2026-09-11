eedb7dd2aff93a246d48ee4e7e750caa6fe1d3c6
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-11 22:45:15 +0300

    Added the login function and data persistence is still there. Need to improve how the user will get the notification that the email does not exist and are required to register

--- flow/Login.py
+++ flow/Login.py
@@ -1,19 +1,23 @@
 from Models.Auth import Auth, AuthenticationError, AuthorizationError
+from Models.DataManager import DataManager
+from pathlib import Path
+
 import getpass
 #Provide access to Auth class
 
 def login():
     try: 
+
         #We simply take the email and password
         email = input ("Please enter your email address: ")
         password = getpass.getpass("Enter your password: ")
 
-        auth = Auth()
-
+        data_manager = DataManager(Path("Data"))
+        auth = Auth(data_manager)
         logged_in_user = auth.login(email, password)
         print(f"Logged in as {logged_in_user.name}")
 
-    except (AuthenticationError, AuthenticationError) as error:
+    except (AuthenticationError, AuthorizationError) as error:
         print(error)
 
 if __name__ == "__main__":
