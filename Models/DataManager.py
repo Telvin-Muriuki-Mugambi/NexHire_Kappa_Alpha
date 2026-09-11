235b4d973b1bcf61c7dafd4ea03b0b67fdd275fb
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-11 17:30:08 +0300

    Created a folder Data to house the user's and job's data in JSON. Initialized the class and a method to ensure the file exists

--- Models/DataManager.py
+++ Models/DataManager.py
@@ -1,1 +1,25 @@
-print("Data Manager class")
+#Data Manager Class: 
+
+#TODO: Add new records
+#TODO: Read records
+#TODO: Save Users in JSON
+#TODO: Save Jobs in JSON
+#TODO: Load Users from JSON
+#TODO: Load Jobs from JSON
+
+from pathlib import Path
+# used for handling and manipulating file and folder paths
+
+class DataManager:
+    #Initializing the class with the directory path to the data
+
+    def __init__(self, data_directory):
+        self.data_directory = Path(data_directory)
+
+    #Ensuring the file exists
+    #It's function is closely related to the class hence use os static method
+    @staticmethod
+    def _ensure_file(path):
+        if not path.exists():
+            #Encodes the text so that the computer may understand
+            path.write_text("[]", encoding = "utf-8")
