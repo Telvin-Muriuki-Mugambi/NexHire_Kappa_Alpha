123387e78244ff40213fddb25888ef67090db239
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-11 23:54:00 +0300

    Added ASCII code for the application logo

--- flow/Landing.py
+++ flow/Landing.py
@@ -1,1 +1,19 @@
-print("This page is used to create a simple landing view on the CLI")
+import colorama
+from colorama import Fore, Style
+
+# Initialize colorama (required for Windows support)
+colorama.init(autoreset=True)
+
+#Use of ASCII to implment the logo
+nexhire_logo = r"""
+ _   _             _   _ _           
+
+| \ | |           | | | (_)          
+|  \| | ___ _   _ | |_| |_ _ __ ___  
+| . ` |/ _ \ \/ / |  _  | | '__/ _ \ 
+| |\  |  __/>  <  | | | | | | |  __/ 
+|_| \_|\___/_/\_\ |_| |_|_|_|  \___| 
+"""
+
+# Fore.CYAN makes the text cyan, Style.BRIGHT makes it vivid
+print(Fore.CYAN + Style.BRIGHT + nexhire_logo)
