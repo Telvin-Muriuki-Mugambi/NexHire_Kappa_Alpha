cdf7ae0428246e868f7a4e9eb418b3bea8626cf8
Telvin-Muriuki-Mugambi <mugambi.telvin.muriuki@gmail.com>
2026-09-11 12:57:13 +0300

    Implemented a function to check if a user is an admin and a method tocheck the authorization of a user

--- Models/Auth.py
+++ Models/Auth.py
@@ -1,9 +1,10 @@
 #Authentication Logic
 
+
 # TODO: Registration of a user
 # TODO: Login of a user
 # TODO: Role Based Access [Job seeker, Employer, Admin]
-# TODO: Password Hashing 
+# TODO: Password Hashing
 
 from Models.User import User
 
@@ -12,49 +13,49 @@ from Models.User import User
 class DuplicateEmailError(ValueError):
     pass
 
-class AuthenitcationError(ValueError):
+
+class AuthenticationError(ValueError):
     pass
+
 
 class AuthorizationError(PermissionError):
     pass
+
 
 class Auth:
     ROLES = {"ADMIN", "JOB_SEEKER", "EMPLOYER"}
 
     def __init__(self):
-        #Used to save the user's data
         self.users = {}
-        #Set the user session
         self.current_user = None
 
     #Method for registering the user
-    def register(self,name,email,phone,password,role="job_seeker"):
-
-        #create an instance of a user
-        user = User (name, email, password) 
-        user.role = role
-        self.users[email] = user
+    def register(self, name, email, phone, password, role="JOB_SEEKER"):
 
         #Check if the email already exists
-        if email  in self.users:
-            raise DuplicateEmailError("The email already exists")
-        
+        if email in self.users:
+            raise DuplicateEmailError("Email is already registered")
         role = role.upper()
+
         #Handle edge case if the role entered exist or not
         if role not in self.ROLES:
-            raise ValueError("Unkown role")
+            raise ValueError("Unknown role")
 
+        #create an instance of a user
+        user = User(name, email, phone, password)
+        user.role = role
+        self.users[email] = user
         return user
+    
     #Method of login of a user. Uses the email and password
-
     def login(self, email, password):
-
         #Retrives the email from the user's dictionary
         user = self.users.get(email)
 
         #Check if the email is there and if the password's match
         if user is None or not user.verify_password(password):
-            raise AuthenitcationError("Invalid email or password")
+            raise AuthenticationError("Invalid email or password")
+        
         #if it passes the current session is set for the current user
         self.current_user = user
         return user
@@ -67,4 +68,14 @@ class Auth:
     #Checks if the user has a role
     def has_role(self, role):
         return self.current_user is not None and self.current_user.role == role.upper()
+
+    #Method to check if the user is an admin
+    def is_admin(self):
+        return self.has_role("ADMIN")
+
+    #Ensure the user has the correct authorization to perform a certain action
+    def require_role(self, role):
+        if not self.has_role(role):
+            raise AuthorizationError("User is not authorized for this action")
+        return True
 
